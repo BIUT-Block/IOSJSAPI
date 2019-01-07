d03ce914cc5f201b2a39a732af38f1b66ce6173a
Yuan Li <abysmli@gmail.com>
2019-01-07 01:02:20 +0100

    Initial Commit

---
+++ Sentimentalist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sentimentalist
+//
+//  Created by Yuan Li on 07.01.19.
+//  Copyright © 2019 Yuan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
